fb2e1cb3b53440c60a5ccc6fba28ad4cc9029def
Ivan Yurkin <yurkin.ivan5456@gmail.com>
2020-02-10 16:31:55 +0300

    Created the basis of the site on node.js

---
+++ app.js
@@ -0,0 +1,24 @@
+let 
+    express = require(`express`),
+    http    = require(`http`),
+    path    = require(`path`),
+    logger  = require(`morgan`);
+
+let app = express();
+
+let indexRouter = require(`./routes/index`);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger("combined"));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(`/`, indexRouter);
+
+let server = http.createServer(app);
+
+server.listen(5050, () => { console.log(`Server is running`) });
