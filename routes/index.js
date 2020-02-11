fb3ea0fad10f575d2e4bcf79fe13975d5f2ae99b
Ivan Yurkin <yurkin.ivan5456@gmail.com>
2020-02-11 09:51:16 +0300

    Added links

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,14 @@ let express = require(`express`);
 let router = express.Router();
 
 router.get(`/`, (req, res) => res.render(`index`));
+router.get(`/lab1`, (req, res) => res.render(`./labs/lab1`));
+router.get(`/lab2`, (req, res) => res.render(`./labs/lab2`));
+router.get(`/lab3`, (req, res) => res.render(`./labs/lab3`));
+router.get(`/lab4`, (req, res) => res.render(`./labs/lab4`));
+router.get(`/lab5`, (req, res) => res.render(`./labs/lab5`));
+router.get(`/lab6`, (req, res) => res.render(`./labs/lab6`));
+router.get(`/lab7`, (req, res) => res.render(`./labs/lab7`));
+router.get(`/lab8`, (req, res) => res.render(`./labs/lab8`));
+router.get(`/lab9`, (req, res) => res.render(`./labs/lab9`));
 
 module.exports = router;
