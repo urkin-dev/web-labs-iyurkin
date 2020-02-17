b221be28893a225d9dc308bb53b6ec5c50495457
Ivan Yurkin <yurkin.ivan5456@gmail.com>
2020-02-18 00:53:54 +0300

    Start to work on lab3

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
 let
-    firstScreen  = document.querySelector(`.first-slide`),
+    firstScreen = document.querySelector(`.first-slide`),
     secondScreen = document.querySelector(`.second-slide`),
-    screenWidth  = document.body.clientWidth,
-    tsStatus     = true, // true when screens aren't moving
-    rsStatus     = false; // window resize status
+    screenWidth = document.body.clientWidth,
+    tsStatus = true, // true when screens aren't moving
+    rsStatus = false; // window resize status
 
-window.onload = function() {
+window.onload = function () {
     document.body.classList.remove(`preload`);
 }
 
@@ -13,7 +13,7 @@ if (screenWidth > 768) {
 
     secondScreen.style.transform = `translateX(${screenWidth}px) scale(.8)`;
 
-    window.onresize = function(e) {
+    window.onresize = function (e) {
         window.location = window.location.href;
     }
 
@@ -24,13 +24,21 @@ if (screenWidth > 768) {
         if (delta > 0 && tsStatus) {
             secondScreen.style.transform = `translateX(0) scale(1)`;
             firstScreen.style.transform = `translateX(-${screenWidth}px) scale(.8)`;
-            secondScreen.ontransitionstart = () => { tsStatus = false }
-            secondScreen.ontransitionend = () => { tsStatus = true }
+            secondScreen.ontransitionstart = () => {
+                tsStatus = false
+            }
+            secondScreen.ontransitionend = () => {
+                tsStatus = true
+            }
         } else if (delta < 0 && tsStatus) {
             secondScreen.style.transform = `translateX(${screenWidth}px) scale(.8)`;
             firstScreen.style.transform = `translateX(0) scale(1)`;
-            secondScreen.ontransitionstart = () => { tsStatus = false }
-            secondScreen.ontransitionend = () => { tsStatus = true }
+            secondScreen.ontransitionstart = () => {
+                tsStatus = false
+            }
+            secondScreen.ontransitionend = () => {
+                tsStatus = true
+            }
         }
     });
 }
