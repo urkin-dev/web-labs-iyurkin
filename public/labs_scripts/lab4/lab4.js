3a6eaab00c5228ddb25fcca20d9037379f36a1e7
Ivan Yurkin <yurkin.ivan5456@gmail.com>
2020-03-03 00:11:19 +0300

    Start to do lab3

---
+++ public/labs_scripts/lab4/lab4.js
@@ -0,0 +1,44 @@
+let
+    nextBtn = document.querySelector('.slider-dots .next'),
+    prevBtn = document.querySelector('.slider-dots .prev'),
+    count   = document.querySelector('.slider-count .count'),
+    slides  = document.getElementsByClassName('slide');
+
+nextBtn.addEventListener('click', next);
+prevBtn.addEventListener('click', prev);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function prev () {
+    showSlides(slideIndex -= 1);
+    count.textContent = slideIndex + '';
+}
+
+function next() {
+    showSlides(slideIndex += 1);
+    count.textContent = slideIndex + '';
+}
+
+function showSlides(n) {
+    // let dots = document.getElementsByClassName('slider-dots-item');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    // for (let i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(' active-dots', '');
+    // }
+
+    slides[slideIndex - 1].style.display = 'block';
+    // dots[slideIndex - 1].classList.add('active-dots');
+}
